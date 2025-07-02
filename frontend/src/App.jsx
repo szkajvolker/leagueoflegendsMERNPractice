e6ba921fd71ef8b6b4e7a1e3427e86035fe2f60e
szkajvolker <ist9595@windowslive.com>
2025-07-02 23:37:01 +0200

    Init ViteReact + basic folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex justify-center items-center border-2 border-green-600 p-2">
+        <h1 className="font-bold">Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
